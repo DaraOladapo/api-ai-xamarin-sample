21a776017edbb3f538a3cfc80b9c610a680b6753
Danil Skachkov <danil.skachkov@speaktoit.com>
2015-03-13 19:00:28 +0600

    Working Android sample

--- AndroidSample/MainActivity.cs
+++ AndroidSample/MainActivity.cs
@@ -22,35 +22,223 @@
 using System;
 
 using Android.App;
-using Android.Content;
-using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+using ApiAiSDK;
+using ApiAi.Common;
+using ApiAi.Android;
+using fastJSON;
+using Android.Util; 
+
 namespace AndroidSample
 {
-	[Activity (Label = "AndroidSample", MainLauncher = true, Icon = "@drawable/icon")]
-	public class MainActivity : Activity
-	{
-		int count = 1;
+    [Activity(Label = "API.AI Sample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private readonly string TAG = typeof(MainActivity).Name;
 
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate (bundle);
+        private ProgressBar progressBar;
+        private ImageView recIndicator;
+        private TextView resultTextView;
 
-			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
+        private EditText contextTextView;
+        private Spinner selectLanguageSpinner;
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
-		}
-	}
+        private AIService aiService;
+
+        LanguageConfig[] languages;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            progressBar = FindViewById<ProgressBar>(Resource.Id.progressBar);
+            recIndicator = FindViewById<ImageView>(Resource.Id.recIndicator);
+            recIndicator.Visibility = ViewStates.Invisible;
+		
+            resultTextView = FindViewById<TextView>(Resource.Id.resultTextView);
+            contextTextView = FindViewById<EditText>(Resource.Id.contextTextView);
+            selectLanguageSpinner = FindViewById<Spinner>(Resource.Id.selectLanguageSpinner);
+
+            languages = new []
+            {
+                new LanguageConfig("en", "c7d7595293f64eda8da1620c81183d00"),
+                new LanguageConfig("ru", "adcb900f02594f4186420c082e44173e"),
+                new LanguageConfig("de", "96807aac0e98426eaf684f4081b7e431"),
+                new LanguageConfig("pt", "4c4a2277516041f6a1c909163ebfed39"),
+                new LanguageConfig("pt-BR", "6076377eea9e4291854204795b55eee9"),
+                new LanguageConfig("es", "430d461ea8e64c09a4459560353a5b1d"),
+                new LanguageConfig("fr", "d6434b3bf49d4a93a25679782619f39d"),
+                new LanguageConfig("it", "4319f7aa1765468194d9761432e4db36"),
+                new LanguageConfig("ja", "6cab6813dc8c416f92c3c2e2b4a7bc27"),
+                new LanguageConfig("ko", "b0219c024ee848eaa7cfb17dceb9934a"),
+                new LanguageConfig("zh-CN", "2b575c06deb246d2abe4bf769eb3200b"),
+                new LanguageConfig("zh-HK", "00ef32d3e35f43178405c046a16f3843"),
+                new LanguageConfig("zh-TW", "dd7ebc8a02974155aeffec26b21b55cf"),
+            };
+            
+            var languagesAdapter = new ArrayAdapter<LanguageConfig>(this, Android.Resource.Layout.SimpleSpinnerDropDownItem, languages);
+            selectLanguageSpinner.Adapter = languagesAdapter;
+            selectLanguageSpinner.ItemSelected += SelectLanguageSpinner_ItemSelected;
+
+            FindViewById<Button>(Resource.Id.buttonListen).Click += buttonListen_Click;
+            FindViewById<Button>(Resource.Id.buttonCancel).Click += buttonCancel_Click;
+            FindViewById<Button>(Resource.Id.buttonStopListen).Click += buttonStopListen_Click;
+        }
+
+        void buttonListen_Click (object sender, EventArgs e)
+        {
+            aiService.StartListening();
+        }
+
+        void buttonStopListen_Click (object sender, EventArgs e)
+        {
+            aiService.StopListening();
+        }
+
+        void buttonCancel_Click (object sender, EventArgs e)
+        {
+            aiService.Cancel();
+        }
+
+        protected override void OnPause()
+        {
+            if (aiService != null)
+            {
+                ((SpeaktoitRecognitionService)aiService).Pause();    
+            }
+
+            base.OnPause();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            if (aiService != null)
+            {
+                ((SpeaktoitRecognitionService)aiService).Resume();    
+            }
+
+        }
+
+        void SelectLanguageSpinner_ItemSelected (object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            
+            var selectedLanguage = languages[e.Position];
+
+            var lang = SupportedLanguage.FromLanguageTag(selectedLanguage.LanguageCode);
+            var config = new AIConfiguration("62f2522a-7404-4c28-b9ac-097ca5d8b32d",
+                selectedLanguage.AccessToken, lang);
+
+            config.DevMode = true;
+
+            if (aiService != null) {
+                aiService.Cancel();
+            }
+
+            aiService = new SpeaktoitRecognitionService(config);
+
+            aiService.OnResult += AiService_OnResult;
+            aiService.OnError += AiService_OnError;
+            aiService.ListeningStarted += AiService_ListeningStarted;
+            aiService.ListeningFinished += AiService_ListeningFinished;
+            aiService.AudioLevelChanged += AiService_AudioLevelChanged;
+        }
+
+        void AiService_OnResult (ApiAiSDK.Model.AIResponse response)
+        {
+            Log.Debug(TAG, "AiService_OnResult");
+            RunOnUiThread(() =>
+                {
+                    if (!response.IsError) {
+
+                        var jsonParams = new JSONParameters { 
+                            UseExtensions = false,
+                            EnableAnonymousTypes = true,
+                            SerializeNullValues = false,
+                        };
+
+                        var responseString = JSON.ToJSON(response, jsonParams);
+                        resultTextView.Text = responseString;
+                    }
+                    else {
+                        resultTextView.Text = response.Status.ErrorDetails;
+                    }
+                }
+            );
+        }
+
+        void AiService_OnError(AIServiceException exception)
+        {
+            Log.Debug(TAG, "AiService_OnError", exception);
+            RunOnUiThread(() =>
+                {
+                    resultTextView.Text = exception.ToString();
+                }
+            );
+        }
+
+        void AiService_AudioLevelChanged (float level)
+        {
+            Log.Debug(TAG, "AiService_AudioLevelChanged");
+            RunOnUiThread(() =>
+                {
+                    float positiveLevel = Math.Abs(level);
+                    if (positiveLevel > 100) {
+                        positiveLevel = 100;
+                    }
+
+                    progressBar.Progress = (int) positiveLevel;
+
+                }
+            );
+        }
+
+        void AiService_ListeningFinished ()
+        {
+            Log.Debug(TAG, "AiService_ListeningFinished");
+            RunOnUiThread(() =>
+                {
+                    recIndicator.Visibility = ViewStates.Invisible;
+                }
+            );
+        }
+
+        void AiService_ListeningStarted ()
+        {
+            Log.Debug(TAG, "AiService_ListeningStarted");
+            RunOnUiThread(() =>
+                {
+                    recIndicator.Visibility = ViewStates.Visible;
+                }
+            );
+        }
+
+    }
+
+    class LanguageConfig
+    {
+        public string LanguageCode { get; set; }
+        public string AccessToken { get; set; }
+
+        public LanguageConfig(string languageCode, string accessToken)
+        {
+            this.LanguageCode = languageCode;
+            this.AccessToken = accessToken;
+        }
+
+
+        public override string ToString()
+        {
+            return LanguageCode;
+        }
+    }
 }
 
 
